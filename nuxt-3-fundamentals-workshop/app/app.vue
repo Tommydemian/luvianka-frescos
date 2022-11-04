89f2490d102f243c477e8a6f91872d1893bab49b
Tomas <tomas@getcharly.com>
2022-11-04 19:20:14 -0300

    first commit

---
+++ nuxt-3-fundamentals-workshop/app/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1>Hello from the best spot in my block</h1>
+  </div>
+</template>
---
+++ nuxt-3-fundamentals-workshop/app/nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+    app: {
+        head: { 
+          title: 'Luvianka-fiambres frescos',
+          meta: [
+            // <meta name="description" content="My amazing site">
+            { name: 'description', content: 'Sitio web sobre cortes de cerdo, fiambres, frescos' }
+          ],
+        }
+      }
+})
